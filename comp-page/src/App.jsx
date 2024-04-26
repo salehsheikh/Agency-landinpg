e719d36b53b0e9549d52de4112bb03a41d8dc079
saleh sheikh <salehsheikh786@gmail.com>
2024-04-26 22:38:57 +0500

    updated

--- comp-page/src/App.jsx
+++ comp-page/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import AboutUs from './pages/AboutUs'
+import Services from './pages/Services'
 
 const App = () => {
   return (
     <>
       <Navbar/>
       <AboutUs/>
+      <Services/>
     </>
   )
 }
