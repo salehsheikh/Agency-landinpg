84489997d32645cc68da68fd593f651cc92faa58
saleh sheikh <salehsheikh786@gmail.com>
2024-05-06 23:01:07 +0500

    updated with routes

--- comp-page/src/App.jsx
+++ comp-page/src/App.jsx
@@ -6,17 +6,25 @@ import CaseStudies from './pages/CaseStudies'
 import Works from './pages/Works'
 import Blog from './pages/Blog'
 import Hire from './pages/Hire'
+import Footer from './components/Footer'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <>
-      <Navbar/>
-      <AboutUs/>
-      <Services/>
-      <CaseStudies/>
-      <Works/>
-      <Blog/>
-      <Hire/>
+     <Navbar/>
+    <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/about' element={<AboutUs/>}/>
+    <Route path='/services' element={ <Services/>}/>
+    <Route path='/casestudies' element={<CaseStudies/>}/>
+    <Route path='/work' element={<Works/>}/>
+    <Route path='/blog' element={ <Blog/>}/>
+    <Route path='/hire' element={ <Hire/>}/>
+    <Route path='*'element={<Home/>}/>
+    </Routes>
+       <Footer/>
     </>
   )
 }
